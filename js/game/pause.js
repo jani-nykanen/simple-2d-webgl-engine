b34c12363d32d9fe2d7898bc9bf86ce7c40f83a2
Jani Nykänen <jnyknn@gmail.com>
2018-07-31 21:43:21 +0300

    Pause box#

--- js/game/pause.js
+++ js/game/pause.js
@@ -5,6 +5,10 @@
 const PAUSE_TIMER_MAX = 20.0;
 const PAUSE_IN = 0;
 const PAUSE_OUT = 1;
+const PAUSE_BOX_WIDTH = 240;
+const PAUSE_BOX_HEIGHT = 320;
+
+const PAUSE_BUTTON_COUNT = 5;
 
 // Pause object
 pause = {};
@@ -16,11 +20,21 @@ pause.timer = 0.0;
 // Pause fade mode
 pause.fadeMode = 0;
 
+// Buttons
+pause.buttons = [];
+
 
 // Initialize
 pause.init = function() {
 
-    // ...
+    // Create buttons
+    tr.fit_view_height(CAMERA_HEIGHT);
+    tr.use_transform();
+    for(var i = 0; i < PAUSE_BUTTON_COUNT; ++ i) {
+
+        // ...
+    }
+
 }
 
 
@@ -49,6 +63,31 @@ pause.update = function(tm) {
 }
 
 
+// Draw the pause box
+pause.draw_box = function() {
+
+    const OUTER_W = 8;
+    const INNER_W = 8;
+
+    let sum = OUTER_W + INNER_W;
+
+    let x = -PAUSE_BOX_WIDTH/2;
+    let y = -PAUSE_BOX_HEIGHT/2;
+
+    let w = PAUSE_BOX_WIDTH;
+    let h = PAUSE_BOX_HEIGHT;
+
+    graph.set_color(0.75, 0.75, 0.75, 1);
+    graph.fill_rectangle(x - sum, y - sum, w + sum*2, h + sum*2);
+
+    graph.set_color(0, 0, 0, 1);
+    graph.fill_rectangle(x - INNER_W, y - INNER_W, w + INNER_W*2, h + INNER_W*2);
+
+    graph.set_color(1, 1, 1, 1);
+    graph.fill_rectangle(x , y, w , h );
+}
+
+
 // Draw pause
 pause.draw = function() {
 
@@ -73,7 +112,16 @@ pause.draw = function() {
     tr.fit_view_height(CAMERA_HEIGHT);
     tr.use_transform();
 
-    // ...
+    // Set transforms for The Box (tm)
+    tr.push();
+    tr.translate(tr.viewport.w / 2, tr.viewport.h / 2);
+    tr.scale(t, t);
+    tr.use_transform();
+
+    // Draw box
+    pause.draw_box();
+
+    tr.pop();
 }
 
 
